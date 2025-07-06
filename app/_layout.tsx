8d7b28a5ea2f31ac4c8a3cbae85edcaac0401b1f
Teds <marktedra29@gmail.com>
2025-07-06 18:02:44 +0800

    do a thing
    Setup appwrite database
    implement env and appwrite lib for the appwrite database conntection project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,10 @@ import {SplashScreen, Stack} from "expo-router";
 import {useFonts} from "expo-font";
 import {useEffect} from "react";
 
+import * as Sentry from '@sentry/react-native';
 import './globals.css'
 
-export default function RootLayout() {
+export default Sentry.wrap(function RootLayout() {
  const [fontLoaded, error] = useFonts({
     "Quicksand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
     "Quicksand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
@@ -13,10 +14,26 @@ export default function RootLayout() {
     "Quicksand-Light": require('../assets/fonts/Quicksand-Light.ttf'),
   });
 
+Sentry.init({
+  dsn: 'https://17b76e167db978284d66add371ced6aa@o4509620930478080.ingest.us.sentry.io/4509620938866688',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
   useEffect(() => {
   if(error) throw error;
   if(fontLoaded) SplashScreen.hideAsync();
   }, [fontLoaded, error]);
 
   return <Stack screenOptions={{headerShown: false}}/>;
-}
+});
